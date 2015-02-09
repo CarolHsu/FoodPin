3e6cf3962d2fbacd59e38b03b0d564ea32986beb
Carol Hsu <sheseee@gmail.com>
2015-02-09 21:14:38 +0800

    Adding "Like" function for restaurant which has been visited

---
+++ FoodPin/CustomTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  CustomTableViewCell.swift
+//  FoodPin
+//
+//  Created by Hsu Carol on 2015/2/7.
+//  Copyright (c) 2015年 Hsu Carol. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var isVisitedImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
